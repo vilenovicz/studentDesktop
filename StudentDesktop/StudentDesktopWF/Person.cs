fc13ee9658eae5c83f5d13a49b66b035fdc71af6
vilenovicz <vilenovicz@yandex.ru>
2019-11-19 17:06:19 +0400

    Person class was added

---
+++ StudentDesktop/StudentDesktopWF/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDesktopWF
+{
+    public class Person
+    {
+        public Person(string lastName, string firstName)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public string LastName { get; private set; }
+        public string FirstName { get; private set; }
+        public DateTime Birthday { get; set; }
+
+        public List<String> Compentencies { get; set; }
+    }
+}
