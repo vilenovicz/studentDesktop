71ad62ed92a77e884662059d0cc68476e30ebb6c
vilenovicz <vilenovicz@yandex.ru>
2019-12-11 21:11:17 +0400

    (!) changed connection String

--- StudentDesktop/StudentDesktopWF/Properties/Settings.Designer.cs
+++ StudentDesktop/StudentDesktopWF/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace StudentDesktopWF.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,11 +26,11 @@ namespace StudentDesktopWF.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\student.mdf;I" +
-            "ntegrated Security=True")]
-        public string studentConnectionString {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=student;Integrated Security=Tr" +
+            "ue")]
+        public string connString {
             get {
-                return ((string)(this["studentConnectionString"]));
+                return ((string)(this["connString"]));
             }
         }
     }
