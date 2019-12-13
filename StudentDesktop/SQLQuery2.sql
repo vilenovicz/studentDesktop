8ff8204290d6a964395729896374ce5437b2ed4b
vilenovicz <vilenovicz@yandex.ru>
2019-12-13 21:46:06 +0400

    (+) CourseForm has been added

--- StudentDesktop/SQLQuery2.sql
+++ StudentDesktop/SQLQuery2.sql
@@ -7,4 +7,17 @@ exec uspNewPerson "Семенов","Семен", "02.12.2019 14:55", 1
 
 --delete from person_competence where id in (13)
 
-select * from Persons
+select * from courses
+
+
+select cp.CourseId, p.LastName, p.FirstName from Persons p inner join course_person cp on p.id = cp.CourseId
+
+select cr.id as id, 
+                cr.Name as [Название курса], 
+                c.Name as [Компетенция],
+                cr.DateFrom [Дата начала],
+                cr.DateTo [Дата окончания]
+                from courses cr inner join Competences c on cr.competenceId = c.id
+
+
+select pc.PersonId, c.Code, c.Name  from Competences c inner join person_competence pc on c.id = pc.CompetenceId
